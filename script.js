e61bb4829c72aabbaee7429a5aaeba7d980a30a4
Rabi <rabi171011@gmail.com>
2025-12-25 07:55:18 +0530

    phase 1

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('getweather');
+const result = document.getElementById('result');
+
+button.addEventListener('click', () => {
+    result.textContent = 'Button clicked! javascript is working';
+});
